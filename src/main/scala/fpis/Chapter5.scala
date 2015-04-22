fab7e718d825856e81c254ec506831ac52ea0d58
Chris Birchall <chris.birchall@gmail.com>
2015-04-22 22:49:13 +0100

    More chapter 5

--- src/main/scala/fpis/Chapter5.scala
+++ src/main/scala/fpis/Chapter5.scala
@@ -53,6 +53,36 @@ object Chapter5 {
 
     def headOption_foldRight: Option[A] =
       foldRight[Option[A]](None)((h, t) => Some(h))
+
+    def map_foldRight[B](f: A => B): Stream[B] =
+      foldRight[Stream[B]](empty)((a, b) => cons(f(a), b))
+
+    def filter_foldRight(p: A => Boolean): Stream[A] =
+      foldRight[Stream[A]](empty)((a, b) => if (p(a)) cons(a, b) else b)
+
+    def append_foldRight[B >: A](other: => Stream[B]): Stream[B] =
+      foldRight[Stream[B]](other)((a, b) => cons(a, b))
+
+    def flatMap_foldRight[B](f: A => Stream[B]): Stream[B] =
+      foldRight[Stream[B]](empty)((a, b) => f(a).append_foldRight(b))
+
+    def map_unfold[B](f: A => B): Stream[B] =
+      unfold(this) {
+        case Cons(h,t) => Some((f(h()), t()))
+        case _ => None
+      }
+
+    def take_unfold[B](n: Int): Stream[A] =
+      unfold(n, this) { case (x, s) =>
+        s match {
+          case Cons(h,t) => 
+            if (x < 1) None
+            else if (x == 1) Some((h(), (0, empty)))
+            else Some((h(), (n-1, t())))
+          case _ => None
+        }
+      }
+
   }
 
   case object Empty extends Stream[Nothing]
@@ -69,6 +99,38 @@ object Chapter5 {
 
     def apply[A](as: A*): Stream[A] =
       if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+
+    def constant[A](a: A): Stream[A] = {
+      lazy val tail: Stream[A] = Cons(() => a, () => tail) 
+      tail
+    }
+
+    def from(n: Int): Stream[Int] = cons(n, from(n + 1))
+
+    def fibs: Stream[Int] = {
+      def loop(i: Int, j: Int): Stream[Int] =
+        cons(i, loop(j, i + j))
+      loop(0, 1)
+    }
+
+    def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
+      case Some((a, s)) => cons(a, unfold(s)(f))
+      case None => empty
+    }
+
+    def fibs_unfold: Stream[Int] =
+      unfold[Int, (Int, Int)]((0, 1)){ case (i, j) => Some((i, (j, i+j))) }
+
+    def from_unfold(n: Int): Stream[Int] =
+      unfold(n)(x => Some((x, x + 1)))
+
+    def constant_unfold[A](a: A): Stream[A] =
+      unfold(a)(x => Some(x, x))
+
+    def ones_unfold[A]: Stream[Int] =
+      unfold(1)(x => Some(x, x))
+
+
   }
 
 
--- src/test/scala/fpis/Chapter5Spec.scala
+++ src/test/scala/fpis/Chapter5Spec.scala
@@ -44,6 +44,60 @@ class Chapter5Spec extends FlatSpec with Matchers {
     Stream.empty[Int].headOption_foldRight should be(None)
     Stream(1).headOption_foldRight should be(Some(1))
   }
+
+  "map_foldRight" should "map the function" in {
+    Stream(1,2,3).map_foldRight(_.toString).toList should be(List("1","2","3"))
+  }
+
+  "filter_foldRight" should "filter the stream" in {
+    Stream(1,2,3).filter_foldRight(_ % 2 == 1).toList should be(List(1,3))
+  }
+
+  "append_foldRight" should "concatenate the streams" in {
+    Stream(1,2,3).append_foldRight(Stream(4,5)).toList should be(List(1,2,3,4,5))
+  }
+
+  "flatMap_foldRight" should "flatMap the function" in {
+    Stream(1,2,3).flatMap_foldRight(_ => Stream("a","b")).toList should be(List("a","b","a","b","a","b"))
+  }
   
+  "constant" should "return an infinite stream of constants" in {
+    Stream.constant(42).take(3).toList should be(List(42,42,42))
+  }
+  
+  "from" should "return an infinite stream of increasing numbers" in {
+    Stream.from(42).take(3).toList should be(List(42,43,44))
+  }
+  
+  "fibs" should "return an infinite stream of Fibonacci numbers" in {
+    Stream.fibs.take(8).toList should be(List(0,1,1,2,3,5,8,13))
+  }
+  
+  "unfold" should "return a stream defined by a function" in {
+    Stream.unfold(0)(s => Some(s, s+1)).take(4).toList should be(List(0,1,2,3))
+  }
+  
+  "fibs_unfold" should "return an infinite stream of Fibonacci numbers" in {
+    Stream.fibs_unfold.take(8).toList should be(List(0,1,1,2,3,5,8,13))
+  }
+  
+  "from_unfold" should "return an infinite stream of increasing numbers" in {
+    Stream.from_unfold(42).take(3).toList should be(List(42,43,44))
+  }
+
+  "constant_unfold" should "return an infinite stream of constants" in {
+    Stream.constant_unfold(42).take(3).toList should be(List(42,42,42))
+  }
+
+  "map_unfold" should "map the function" in {
+    Stream(1,2,3).map_unfold(_.toString).toList should be(List("1","2","3"))
+  }
+  
+  "take_unfold" should "return a Stream of the first n elements" in {
+    Stream(1,2,3).take_unfold(0).toList should be(Nil)
+    Stream(1,2,3).take_unfold(2).toList should be(List(1,2))
+    Stream(1,2,3).take_unfold(5).toList should be(List(1,2,3))
+  }
+
 
 }
